d7416a569568f6d00a66d1ef4f672ddd15ee5d4c
developerMohib <mohibullahmohim2020@gmail.com>
2025-01-03 12:21:01 +0600

    redux initialize

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { store } from './redux/store.ts'
+import { Provider } from 'react-redux'
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Root element not found.");
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
